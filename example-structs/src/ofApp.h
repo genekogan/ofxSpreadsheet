5d6732a67d94fb0fef3044e697f9ba1da9afb70e
DHaylock <david.haylock@watershed.co.uk>
2016-01-17 14:22:06 +0000

    - Added: Src for example

--- example-structs/src/ofApp.h
+++ example-structs/src/ofApp.h
@@ -1,7 +1,34 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxSpreadsheet.h"
 
+struct myStruct {
+    
+    myStruct(int _entryID,int _customerID,string _customerFirstname,string _customerSurname,float _balance) {
+        entryID = _entryID;
+        customerID = _customerID;
+        customerFirstname = _customerFirstname;
+        customerSurname = _customerSurname;
+        balance = _balance;
+    }
+    
+    vector <string> getDetails() {
+        vector<string> detail;
+        detail.push_back(ofToString(entryID));
+        detail.push_back(ofToString(customerID));
+        detail.push_back(customerFirstname);
+        detail.push_back(customerSurname);
+        detail.push_back(ofToString(balance));
+        return detail;
+    }
+    
+    int entryID;
+    int customerID;
+    string customerFirstname;
+    string customerSurname;
+    float balance;
+};
 class ofApp : public ofBaseApp{
 
 	public:
@@ -20,5 +47,8 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
+        ofxSpreadsheet spreadsheet;
+        bool reverse;
+        vector <myStruct> details;
 };
